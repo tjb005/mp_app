5aae4e4ea5eb93d3f94ca8ad9fdb0937246b4fb2
Tim B <tim.biason@gmail.com>
2014-09-17 11:25:57 +1000

    20140917

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -3,9 +3,25 @@ class AircraftsController < ApplicationController
   end
 
   def show
+    # Models
   	@aircraft = Aircraft.find(params[:id])
   	@w1s = @aircraft.w1s
   	@w1c1s = @aircraft.w1c1s
+
+    @w1_history =[]
+    @date_point = (1.year.ago.to_date..Date.today) 
+
+    @date_point.each do |date_point|
+      
+        @w1s.each do |w1|
+          if w1.date == w1.date
+           @test = date_point - w1.date
+           @w1_history.push(@test)
+          end
+        end
+
+    end
+
   end
 
   def index
